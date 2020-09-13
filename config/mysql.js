40b1b10382ad6a1b179d5404598e6617b1787531
pipe <1173882893@qq.com>
2020-09-13 16:48:42 +0800

    优化封装mysql，抛出异常。集成参数校验

--- config/mysql.js
+++ config/mysql.js
@@ -1,36 +1,30 @@
-//导入所需模块  
-var mysql=require("mysql");    
-//导入配置文件  
-var cfg  =require("./database");  
-var pool = mysql.createPool({    
-    host:      cfg.HOST,  
-    user:      cfg.USER,   
-    password:  cfg.PASS,    
-    database:  cfg.DATABASE,    
-    port:      cfg.PORT  
-});    
-//导出查询相关  
+//导入所需模块
+var mysql = require("mysql");
+//导入配置文件
+var cfg = require("./database");
+var pool = mysql.createPool({
+  host: cfg.HOST,
+  user: cfg.USER,
+  password: cfg.PASS,
+  database: cfg.DATABASE,
+  port: cfg.PORT,
+});
+//导出查询相关
 //封装mysql
-var query=function(sql,params){    
-  return new Promise((resolve,reject)=>{
-    pool.getConnection(function(err,conn){    
-       if(err){    
-           //失败事件驱动回调 
-            reject(err,null,null);    
-        }else{    
-            conn.query(sql,params,function(qerr,vals,fields){    
-                //释放连接   
-                conn.release();    
-                console.log()
-                //成功事件驱动回调    
-                if(!qerr){
-                resolve(vals);  
-                }else{
-                     reject(qerr);   
-                }
-            });    
-        }  
-        }) 
-    });    
-};    
-module.exports=query;    
+var query = async (sql, params) => {
+  try {
+    return await new Promise((resolve, reject) => {
+      pool.getConnection((err, conn) => {
+        conn.query(sql, params, function (qerr, vals, fields) {
+          //释放连接
+          conn.release();
+          //成功事件驱动回调
+          resolve(vals);
+        });
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = query;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,40 @@
 var express = require("express");
 var router = express.Router();
 var query = require("../config/mysql");
-var jwt = require('jsonwebtoken');
+var jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+var createError = require("http-errors");
 /* GET home page. */
-router.get("/login", function (req, res, next) {
-debugger
-  query("select * from users where username=?",['si'])
-    .then((res1) => {
-      debugger
-   let token=   jwt.sign({
-        id:res1[0].id,
-        username:res1[0].username
-          }, 'hahaha', {
-            expiresIn: '1d' 
-          })
-          res.json({token:token,...res1[0]})
-    })
-    .catch((err) => {
-      console.log(err)
-      res.json(err)
-    });
-});
+router.get(
+  "/login",
+  [body("username").exists().withMessage("不存在")],
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      let test = createError(400, errors);
+      return res.status(test.status).json({ ...test });
+    }
+    try {
+      const user = req.body;
+      let res1 = await query(
+        "select * from users where username=? and password=?",
+        [user.username, user.password]
+      );
+      let token = jwt.sign(
+        {
+          id: res1[0].id,
+          username: res1[0].username,
+        },
+        "hahaha",
+        {
+          expiresIn: "1d",
+        }
+      );
+      res.json({ token: token, ...res1[0] });
+    } catch (error) {
+      res.send(error);
+    }
+  }
+);
 
 module.exports = router;
