a728c1c4e59a06d59c4d4ea860e6933adbe36634
pipe <1173882893@qq.com>
2020-09-22 17:39:45 +0800

    集中修复

--- routes/users.js
+++ routes/users.js
@@ -1,20 +1,23 @@
 var express = require("express");
 var router = express.Router();
 var jwt = require("jsonwebtoken");
-let query =require('../config/mysql')
+let query = require("../config/mysql");
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  let token = req.body.token
+  let token = req.body.token;
   //解析token
-  jwt.verify(token, "hahaha",(err,value)=>{
-    res.json(value)
+  jwt.verify(token, "hahaha", (err, value) => {
+    res.json(value);
   });
 });
-router.get("/addUser", async (req, res, next)=> {
-  const username ='xiaomi'
-  const password ='1234'
-  let s=await query("insert into users (username,password) values (?,?)",[username,password])
-  debugger
-res.json('cheng')
+router.get("/addUser", async (req, res, next) => {
+  const username = "xiaomi";
+  const password = "1234";
+  let s = await query("insert into users (username,password) values (?,?)", [
+    username,
+    password,
+  ]);
+  debugger;
+  res.json("cheng");
 });
 module.exports = router;
